79b599ecd0040e685aaf2e411d411e60d902a498
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-19 08:24:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+class BoardMember{
+  constructor (name, homeState, training){
+    this.name= name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+
+  boardMember.prototype.veto = function(){
+    returns `No, I must disagree`
+  }
+}
