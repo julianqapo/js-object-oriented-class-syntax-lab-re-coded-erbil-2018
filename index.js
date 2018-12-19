8daf4a869c4397504cadf468f9bf1ebcc901af1e
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-19 08:57:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,15 @@ doCharity(){
 
 releasePressStatement(){
   return `You will see great things from Scuber.`
-} 
+}
 
 sayHi(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
+}
+
+class Ceo extends boardMember {
+  hireEmployee(){
+    return `Welcome aboard!`
+  }
 }
